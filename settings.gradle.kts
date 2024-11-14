0f0079bb03d5fd2ce3dc1656f4de9d0c2489b73c
t02690 <fernandotupacyupanqui@bcp.com.pe>
2024-11-14 16:12:58 -0500

    feat: add new modules

---
+++ settings.gradle.kts
@@ -0,0 +1,20 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "convetionsplugins"
+include(":app")
+include(":feature1")
+include(":feature2")
+include(":feature3")
